5172f6e6bf390d31c24ad675e634c685dfea9f26
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2020-10-31 21:38:56 +0100

    Complete implementation without optional behavior

--- spec/basicSpec.js
+++ spec/basicSpec.js
@@ -5,23 +5,50 @@
 const pino = require('pino')
 const memfs = require('memfs')
 const Volume = memfs.Volume
+const readadir = require('../lib/read-a-dir')
 
-const logger = pino({ level: process.env['SSTRUCT_BASICSPEC_LOG_LEVEL'] || 'info' })
+const logger = pino({ level: process.env['READADIR_BASICSPEC_LOG_LEVEL'] || 'info' })
 
-describe('readdir function', function() {
-  it('finds a file', function() {
+const files = [
+  '1.txt',
+  '2/2.txt',
+  '2/2.1.txt',
+  '3.txt'
+]
+
+describe('readadir function', function() {
+
+  function f (dir) {
     return new Promise(function (resolve, reject) {
-      logger.debug({ messsage: 'Basic test' })
+      logger.debug({ message: 'Basic test' })
       try {
         const vol = new Volume()
-        vol.writeFileSync('/example.txt', 'Example text')
+        const json = {}
+        files.forEach(x => json[x] = 'Contenst of ' + x)
+        vol.fromJSON(json, dir)
         resolve(vol)
       } catch (error) {
         reject(error)
       }
     })
-      .then(() => {
-        expect(true).toEqual(true)
+      .then(vol => {
+        return readadir.readadir(dir, { fs: memfs.createFsFromVolume(vol) })
       })
+      .then(r => {
+        logger.debug({ result: r })
+        expect(r.sort()).toEqual(files.sort())
+      })
+  }
+
+  it('finds files in a directory', function() {
+    f('/directory')
+  })
+
+  it('finds files in a subdirectory', function() {
+    f('/sub/directory')
+  })
+
+  it('finds files in at root /', function() {
+    f('/')
   })
 })
